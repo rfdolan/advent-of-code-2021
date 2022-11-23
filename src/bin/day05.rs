c1bf717591d03da0bf194c61b9826edbd9d2f76f
Raymond <rfdolan@wpi.edu>
2022-11-22 23:13:59 -0800

    Fix issue with day 5

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -96,16 +96,7 @@ fn part2(vec: &Vec<String>) {
             //let t = time::Duration::from_secs(1);
             //std::thread::sleep(t);
             if x_it as u32 == x2 && y_it as u32 == y2  { break;}
-            if x_it as u32 != x2  { x_it += x_dir;}0,9 -> 5,9
-            8,0 -> 0,8
-            9,4 -> 3,4
-            2,2 -> 2,1
-            7,0 -> 7,4
-            6,4 -> 2,0
-            0,9 -> 2,9
-            3,4 -> 1,4
-            0,0 -> 8,8
-            5,5 -> 8,2
+            if x_it as u32 != x2  { x_it += x_dir;}
             if y_it as u32 != y2  { y_it += y_dir;}
         }
     }
